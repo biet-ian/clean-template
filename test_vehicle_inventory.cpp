47eb8a944ced2ddab67c8b299475768c783150ef
biet-ian <nitinkv@outlook.com>
2020-08-04 16:35:09 +0530

    corrected test case

--- test_vehicle_inventory.cpp
+++ test_vehicle_inventory.cpp
@@ -12,7 +12,7 @@ TEST(COLLECT, verify_storage_incoming_status_of_a_vehicle )
     inventory_entry = telematics_to_inventory( 33, measure::battery_pc, 99.99);
     ASSERT_NEAR (95.0, inventory_entry[measure::motor_temp], 0.01);
     ASSERT_NEAR (99.99, inventory_entry[measure::battery_pc], 0.001);    
-    ASSERT_TRUE( std::isnan(inventory_entry[measure::battery_pc]) );
+    ASSERT_TRUE( std::isnan(inventory_entry[measure::battery_temp]) );
 }
 
 int main(int argc, char **argv) {
